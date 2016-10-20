52fce9422f2b276be35ea59ea5b6e54482e537f0
Geir <ulvik@usit.uio.no>
2016-10-20 21:56:08 +0200

    Writing data to file works.

--- project_3/main.cpp
+++ project_3/main.cpp
@@ -9,11 +9,26 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int N = atoi(argv[1]);
-i   //I denne filen driver Geir med fjasing.
-    //units [m] = solar masses, [r] = au, [v] = au/day
+    //units [m] = solar masses, [r] = au, [v] = au/yr
     SolarSystem incandescence;
 
     CelestialBody &solaire = incandescence.createCelestialBody(vec3(0, 0, 0), vec3(0,0,0), 1);
     CelestialBody &holy_terra = incandescence.createCelestialBody(vec3(1, 0, 0), vec3(0, 2*M_PI, 0), 3e-6);
+
+    vector<CelestialBody> &bodies = incandescence.bodies();
+
+    for(int i = 0; i<bodies.size(); i++) {
+        CelestialBody &body = bodies[i]; // Reference to this body
+        cout << "The position of this object is " << body.position << " with velocity " << body.velocity << endl;
+    }
+
+    double dt = 0.001;
+    Euler integrator(dt);
+    for(int timestep=0; timestep<N; timestep++) {
+            integrator.integrateOneStep(incandescence);
+            incandescence.writeToFile("positions.xyz");
+    }
+
+    cout << "I just created my first solar system that has " << incandescence.bodies().size() << " objects." << endl;
     return 0;
 }
